bab05c5df7d2bfc8c9690a7320af1419fc733414
Ecconia <public-commit@ecconia.com>
2018-12-13 17:33:08 +0100

    Adding plenty more packet names to the play handler
    Just the names, no feature change.
    Chunk packets censored cause too many in a short time.

--- src/de/ecconia/mc/jclient/PlayPacketHandler.java
+++ src/de/ecconia/mc/jclient/PlayPacketHandler.java
@@ -24,29 +24,28 @@ public class PlayPacketHandler implements PacketHandler
 		{
 			Provider p = new ArrayProvider(bytes);
 			int id = p.readCInt();
-			System.out.println(">>> Packet with ID:" + id + " Size:" + p.remainingBytes());
 			
 			//State for play packet.
 			if(id == 3)
 			{
-				System.out.println("Server requests compression:");
+				System.out.println(">>> Packet: Compression request");
 				int compressionLevel = p.readCInt();
-				System.out.println("Compression above " + compressionLevel + " bytes.");
+				System.out.println("> Compression above " + compressionLevel + " bytes.");
 				if(p.remainingBytes() > 0)
 				{
-					System.out.println("WARNING: Compression package had more content.");
+					System.out.println("> WARNING: Compression package had more content.");
 				}
 				
 				con.setCompression(compressionLevel);
 			}
 			else if(id == 27)
 			{
-				System.out.println("Packet: Kick!");
-				System.out.println("Message: " + p.readString());
+				System.out.println(">>> Packet: Disconnected by server");
+				System.out.println("> Message: " + p.readString());
 			}
 			else if(id == 33)
 			{
-				System.out.println("Packet: KeepAlive");
+				System.out.println(">>> Packet: Ping");
 				byte[] ping = p.readBytes(8);
 				
 				MessageBuilder mb = new MessageBuilder();
@@ -56,17 +55,153 @@ public class PlayPacketHandler implements PacketHandler
 			}
 			else if(id == 14)
 			{
-				System.out.println("Packet: Chat");
+				System.out.println(">>> Packet: Chat");
 				String jsonMessage = p.readString();
-				System.out.println("Json: " + jsonMessage);
-				System.out.println("Loc: " + p.readByte());
+				System.out.println("> Json: " + jsonMessage);
+				System.out.println("> Loc: " + p.readByte());
 				
 				dataDude.newChatJSON(jsonMessage);
+			}
+			else if(id == 74)
+			{
+				System.out.println(">>> Packet: WorldTime");
+				
+			}
+			else if(id == 13)
+			{
+				System.out.println(">>> Packet: Server difficulty");
+				//UByte -> difficulty
+			}
+			else if(id == 0x2e)
+			{
+				System.out.println(">>> Packet: Player abilities");
+				//Byte -> Invunerable/Flying/AllowFlying/InstaBreak
+				//Float -> Fly speed
+				//Float -> FOV (movement speed)
+			}
+			else if(id == 0x3d)
+			{
+				System.out.println(">>> Packet: Inventory Slot selected");
+			}
+			else if(id == 0x54)
+			{
+				System.out.println(">>> Packet: Declare Receipts (big packet)");
+				//CInt -> Amount
+				//[
+				//String -> Identifier
+				//String -> Where
+				//.....
+				//]
+			}
+			else if(id == 0x55)
+			{
+				System.out.println(">>> Packet: \"Tags\"");
+				//Mapping for BlockID <-> BlockIdentifier?
+			}
+			else if(id == 0x1c)
+			{
+				System.out.println(">>> Packet: Entity status");
+			}
+			else if(id == 0x11)
+			{
+				System.out.println(">>> Packet: Declare command (:@)");
+			}
+			else if(id == 0x34)
+			{
+				System.out.println(">>> Packet: Unlock receipts");
+			}
+			else if(id == 0x47)
+			{
+				System.out.println(">>> Packet: Teams");
+			}
+			else if(id == 0x4e)
+			{
+				System.out.println(">>> Packet: Header/Footer PlayerList");
+			}
+			else if(id == 0x30)
+			{
+				System.out.println(">>> Packet: Player entry");
+			}
+			else if(id == 0x3F)
+			{
+				System.out.println(">>> Packet: Entity metadata");
+			}
+			else if(id == 0x22)
+			{
+//				System.out.println(">>> Packet: Chunk Data");
+				//Obmitted, cause faster boot.
+			}
+			else if(id == 0x02)
+			{
+				System.out.println(">>> Packet: Spawn entity (thunderbolt)???");
+				//CInt -> EntityID
+				//Byte -> Type
+				//Double -> x
+				//Double -> y
+				//Double -> z
+			}
+			else if(id == 0x44)
+			{
+				System.out.println(">>> Update health");
+			}
+			else if(id == 0x43)
+			{
+				System.out.println(">>> Set XP");
+			}
+			else if(id == 25)
+			{
+				System.out.println(">>> Packet: \"Plugin\" Message");
+				//String -> channel
+				//Byte[] -> data
+			}
+			else if(id == 0x51)
+			{
+				System.out.println(">>> Advancements");
+				//Dough - big one
+			}
+			else if(id == 0x52)
+			{
+				System.out.println(">>> Entity Properties");
+			}
+			else if(id == 0x15)
+			{
+				System.out.println(">>> Window item update");
+			}
+			else if(id == 0x17)
+			{
+				System.out.println(">>> Set Inventory Slot");
+			}
+			else if(id == 0x49)
+			{
+				System.out.println(">>> Spawn Position");
+			}
+			else if(id == 0x32)
+			{
+				System.out.println(">>> Player Location/Head");
+			}
+			else if(id == 0x3b)
+			{
+				System.out.println(">>> World Border");
+			}
+			else if(id == 37)
+			{
+				System.out.println(">>> Packet: JoinPlayerState");
+				//Int -> ID
+				//UByte -> Gamemode
+				//Int -> Dimension Type
+				//UByte -> Difficulty
+				//UByte -> Max players (ignored)
+				//String -> Map type...?
+				//Boolean -> Show Debug
+			}
+			else
+			{
+				System.out.println("||> Packet: ID:" + id + "(0x" + (Integer.toHexString(id)) + ")" + " Size:" + p.remainingBytes());
 			}
 		}
 		catch(Exception e)
 		{
-			System.out.println("ERROR: Exception while reading packet: " + e.getClass().getSimpleName() + " - " + e.getMessage());
+			System.out.println("> ERROR: Exception while reading packet: " + e.getClass().getSimpleName() + " - " + e.getMessage());
 			e.printStackTrace(System.out);
 		}
 	}
