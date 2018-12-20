7e3fcb319c66a8ea11fb07e548ba2f39e44dad42
Ecconia <public-commit@ecconia.com>
2018-12-20 18:28:04 +0100

    Fix: Do not handle compression requests outside of the reading thread!
    Caused the first packets to be corrupted until compression was enabled.

--- src/main/java/de/ecconia/mc/jclient/network/PlayPacketHandler.java
+++ src/main/java/de/ecconia/mc/jclient/network/PlayPacketHandler.java
@@ -1,6 +1,7 @@
 package de.ecconia.mc.jclient.network;
 
 import de.ecconia.mc.jclient.PrimitiveDataDude;
+import de.ecconia.mc.jclient.gui.monitor.L;
 import de.ecconia.mc.jclient.network.connector.Connector;
 import de.ecconia.mc.jclient.network.connector.PacketHandler;
 import de.ecconia.mc.jclient.network.packeting.GenericPacket;
@@ -12,9 +13,11 @@ public class PlayPacketHandler implements PacketHandler
 {
 	private final PacketThread genericThread;
 	private final PacketThread pingThread;
+	private final Connector con;
 	
 	public PlayPacketHandler(Connector con, PrimitiveDataDude dataDude)
 	{
+		this.con = dataDude.getCon();
 		this.pingThread = new PingPacketProcessor("PinfPacketThread", dataDude);
 		this.genericThread = new GenericPacketProcessor("GenericPacketThread", dataDude);
 	}
@@ -29,7 +32,19 @@ public class PlayPacketHandler implements PacketHandler
 			
 			GenericPacket packet = new GenericPacket(id, p);
 			
-			if(id == 33)
+			if(id == 3)
+			{
+				logPacket("Compression request");
+				int compressionLevel = p.readCInt();
+				logData("> Compression above " + compressionLevel + " bytes.");
+				if(p.remainingBytes() > 0)
+				{
+					logData("> WARNING: Compression package had more content.");
+				}
+				
+				con.setCompression(compressionLevel);
+			}
+			else if(id == 33)
 			{
 				//Priority packet, has to be handled as fast as possible without anything in the way.
 				pingThread.handle(packet);
@@ -45,5 +60,16 @@ public class PlayPacketHandler implements PacketHandler
 			System.out.println("> ERROR: Exception while reading packet: " + e.getClass().getSimpleName() + " - " + e.getMessage());
 			e.printStackTrace(System.out);
 		}
+	}
+	
+	private void logPacket(String name)
+	{
+//		System.out.println(">>> P: " + name);
+		L.writeLineOnChannel("Gen: Packets", name);
+	}
+	
+	private void logData(String message)
+	{
+		L.writeLineOnChannel("Content", message);
 	}
 }
