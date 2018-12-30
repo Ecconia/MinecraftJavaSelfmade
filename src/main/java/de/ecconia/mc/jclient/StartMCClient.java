3211cbc83ed284e3daa9e17090c9e9ce0028f452
Ecconia <public-commit@ecconia.com>
2018-12-30 18:10:48 +0100

    Adding helper object for "serverlist"

--- src/main/java/de/ecconia/mc/jclient/StartMCClient.java
+++ src/main/java/de/ecconia/mc/jclient/StartMCClient.java
@@ -7,18 +7,20 @@ import old.packet.MessageBuilder;
 
 public class StartMCClient
 {
+	private static CD cd = null;
+	
 	public static void main(String[] args)
 	{
 		Credentials.load();
 		L.init();
+
+		cd = new CD("s.redstone-server.info");
+//		cd = new CD("localhost");
 		
-		String domain = "s.redstone-server.info";
-//		String domain = "localhost";
-		
-		Connector con = new Connector(domain, 25565, (connector) ->  {
+		Connector con = new Connector(cd.domain, cd.port, (connector) ->  {
 			MessageBuilder mb = new MessageBuilder();
 			
-			mb.addCInt(404);
+			mb.addCInt(cd.version);
 			mb.addString("The-Cake-Is-A-Lie-Or-The-URL-I-Choose");
 			mb.addShort(666);
 			mb.addCInt(2);
@@ -37,5 +39,34 @@ public class StartMCClient
 		
 		con.setHandler(new LoginPacketHandler(con, dataDude));
 		con.connect();
+	}
+	
+	private static class CD
+	{
+		public String domain;
+		public int port = 25565;
+		public int version = 404;
+		
+		public CD(String domain)
+		{
+			int index = domain.indexOf(':');
+			if(index == -1)
+			{
+				this.domain = domain;
+			}
+			else
+			{
+				this.domain = domain.substring(0, index);
+				this.port = Integer.parseInt(domain.substring(index + 1));
+			}
+		}
+		
+		@SuppressWarnings("unused") //May be used may not be used.
+		public CD(String domain, int version)
+		{
+			this(domain);
+			
+			this.version = version;
+		}
 	}
 }
