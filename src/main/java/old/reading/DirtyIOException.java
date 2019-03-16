d465cc67c0e8db92d9d31bf04742f3068e486632
Ecconia <public-commit@ecconia.com>
2019-03-16 17:29:06 +0100

    Fix: Properly set cause of exception wrapper

--- src/main/java/old/reading/DirtyIOException.java
+++ src/main/java/old/reading/DirtyIOException.java
@@ -5,15 +5,8 @@ import java.io.IOException;
 @SuppressWarnings("serial")
 public class DirtyIOException extends RuntimeException
 {
-	private final IOException x;
-	
 	public DirtyIOException(IOException x)
 	{
-		this.x = x;
-	}
-	
-	public IOException getException()
-	{
-		return x;
+		super(x);
 	}
 }
